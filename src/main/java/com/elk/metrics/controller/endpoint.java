ee5039e437133da3abf7017a68722ab57873ff25
Bruno Silva <bruno_bls23@hotmail.com>
2022-10-17 12:11:41 -0300

    Update DMC configuration
    Edit configuration MDC Fields

--- src/main/java/com/elk/metrics/controller/endpoint.java
+++ src/main/java/com/elk/metrics/controller/endpoint.java
@@ -2,20 +2,23 @@ package com.elk.metrics.controller;
 
 import java.util.Random;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
+import org.springframework.util.StringUtils;
+
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.slf4j.MDC;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j;
 
-// @Slf4j
+@Slf4j
 @RestController
 public class endpoint {
 
-    private static final Logger logger = LoggerFactory.getLogger(endpoint.class);
+    // private static final Logger logger = LoggerFactory.getLogger(endpoint.class);
     
     @GetMapping("/")
     public String index(){
@@ -23,11 +26,11 @@ public class endpoint {
 
         MDC.put("logs.id_algo", "sdfasa");
 
-        logger.trace("TRACE"); // Log do Lombox, que vem pela anotação @Log4j2
-        logger.debug("debug"); // Log da instancia do logger
-        logger.info("info");
-        logger.warn("warn");
-        logger.error("error");
+        log.trace("TRACE"); // Log do Lombox, que vem pela anotação @Log4j2
+        log.debug("debug"); // Log da instancia do logger
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
         return "Loggin Spring Boot";
     }  
 }
