80582ec70f80cf5302c7ceb9672f8acc862e1c34
Bruno Silva <bruno_bls23@hotmail.com>
2022-11-10 13:49:27 -0300

    new tests

--- src/main/java/com/elk/metrics/controller/endpoint.java
+++ src/main/java/com/elk/metrics/controller/endpoint.java
@@ -2,6 +2,8 @@ package com.elk.metrics.controller;
 
 import java.util.Random;
 
+// import java.util.Random;
+
 import org.slf4j.MDC;
 import org.springframework.util.StringUtils;
 
@@ -22,9 +24,20 @@ public class endpoint {
     
     @GetMapping("/")
     public String index(){
-        MDC.put("logs.id_cliente", new Random().toString());
+        Random rand = new Random();
+
+        // nextInt as provided by Random is exclusive of the top value so you need to add 1 
+
+        int randomNum = rand.nextInt((5 - 0) + 1) + 0;
+
+        MDC.put("logs.id_cliente", Integer.toString(randomNum+15));
 
         MDC.put("logs.id_algo", "sdfasa");
+        MDC.put("qtd_cavalos",  Integer.toString(randomNum+1));
+        MDC.put("qtd_poneis", Integer.toString(randomNum));
+
+        MDC.put("tipo_documento", "CTe");
+        MDC.put("UF_documento", "PR");
 
         log.trace("TRACE"); // Log do Lombox, que vem pela anotação @Log4j2
         log.debug("debug"); // Log da instancia do logger
